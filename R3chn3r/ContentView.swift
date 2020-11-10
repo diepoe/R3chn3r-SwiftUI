22da4d8a06f9c563db1349d2ea974079426b751d
Dietrich <dietrich.poensgen@heg-portal.de>
2020-11-10 21:50:43 +0100

    create functioning buttons and input

--- R3chn3r/ContentView.swift
+++ R3chn3r/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var rechnung: String=""
+   
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Form {
+                HStack {
+                    TextField("Rechnung", text: $rechnung)
+                    Button(action: {self.rechnung += "="}){Text("=")}
+                }
+            }
+            HStack {
+                Button(action: {self.rechnung += "7"}){Text("7")}
+                Spacer()
+                Button(action: {self.rechnung += "8"}){Text("8")}
+                Spacer()
+                Button(action: {self.rechnung += "9"}){Text("9")}
+            
+            }
+            Spacer()
+            HStack {
+                Button(action: {self.rechnung += "4"}){Text("4")}
+                Spacer()
+                Button(action: {self.rechnung += "5"}){Text("5")}
+                Spacer()
+                Button(action: {self.rechnung += "6"}){Text("6")}
+            }
+            Spacer()
+            HStack {
+                Button(action: {self.rechnung += "1"}){Text("1")}
+                Spacer()
+                Button(action: {self.rechnung += "2"}){Text("2")}
+                Spacer()
+                Button(action: {self.rechnung += "3"}){Text("3")}
+            }
+            Spacer()
+        }
     }
 }
 
