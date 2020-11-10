e198478328f61d1cb4579fc24aba0cba144a4e92
Dietrich <dietrich.poensgen@heg-portal.de>
2020-11-10 21:01:22 +0100

    Initial Commit

---
+++ R3chn3r/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  R3chn3r
+//
+//  Created by Dietrich Poensgen on 10.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ R3chn3r/R3chn3rApp.swift
@@ -0,0 +1,17 @@
+//
+//  R3chn3rApp.swift
+//  R3chn3r
+//
+//  Created by Dietrich Poensgen on 10.11.20.
+//
+
+import SwiftUI
+
+@main
+struct R3chn3rApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
