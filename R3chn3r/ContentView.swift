013423279e9082352ff8c7bb97c13c984865a7a9
Dietrich <dietrich.poensgen@heg-portal.de>
2020-11-11 11:40:15 +0100

    Create first button style for the number buttons and apply it

--- R3chn3r/ContentView.swift
+++ R3chn3r/ContentView.swift
@@ -7,6 +7,20 @@
 
 import SwiftUI
 
+struct NumberButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .background(Color(.white))
+            .foregroundColor(Color(.darkGray))
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            .padding()
+            .frame(maxWidth: .infinity)
+            
+            
+            
+    }
+}
+
 struct ContentView: View {
     @State var rechnung: String=""
    
@@ -20,28 +34,36 @@ struct ContentView: View {
                 }
             }
             HStack {
-                Button(action: {self.rechnung += "7"}){Text("7")}
-                Spacer()
-                Button(action: {self.rechnung += "8"}){Text("8")}
-                Spacer()
-                Button(action: {self.rechnung += "9"}){Text("9")}
+                Button(action: {self.rechnung += "7"}){Text("7")}.buttonStyle(NumberButtonStyle())
+                
+                Button(action: {self.rechnung += "8"}){Text("8")}.buttonStyle(NumberButtonStyle())
+                
+                Button(action: {self.rechnung += "9"}){Text("9")}.buttonStyle(NumberButtonStyle())
             
             }
             Spacer()
             HStack {
-                Button(action: {self.rechnung += "4"}){Text("4")}
-                Spacer()
-                Button(action: {self.rechnung += "5"}){Text("5")}
-                Spacer()
-                Button(action: {self.rechnung += "6"}){Text("6")}
+                Button(action: {self.rechnung += "4"}){Text("4")}.buttonStyle(NumberButtonStyle())
+                
+                Button(action: {self.rechnung += "5"}){Text("5")}.buttonStyle(NumberButtonStyle())
+                
+                Button(action: {self.rechnung += "6"}){Text("6")}.buttonStyle(NumberButtonStyle())
             }
             Spacer()
             HStack {
-                Button(action: {self.rechnung += "1"}){Text("1")}
+                Button(action: {self.rechnung += "1"}){Text("1")}.buttonStyle(NumberButtonStyle())
+                
+                Button(action: {self.rechnung += "2"}){Text("2")}.buttonStyle(NumberButtonStyle())
+                
+                Button(action: {self.rechnung += "3"}){Text("3")}.buttonStyle(NumberButtonStyle())
+            }
+            Spacer()
+            HStack {
                 Spacer()
-                Button(action: {self.rechnung += "2"}){Text("2")}
+                
+                Button(action: {self.rechnung += "0"}){Text("0")}.buttonStyle(NumberButtonStyle())
+                
                 Spacer()
-                Button(action: {self.rechnung += "3"}){Text("3")}
             }
             Spacer()
         }
